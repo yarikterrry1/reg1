ad04acb7c74a644bfccef834d110a5906bb5cb53
yarikterrry1 <56186390+yarikterrry1@users.noreply.github.com>
2023-05-10 13:41:42 +0500

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,32 @@
+<?php 
+    session_start();
+    include_once "config.php";
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    if(!empty($email) && !empty($password)){
+        $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}' OR lname = '{$email}'");
+        if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+            $user_pass = md5($password);
+            $enc_pass = $row['password'];
+            if($user_pass === $enc_pass){
+                $status = "Active now";
+                $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+                if($sql2){
+                    $_SESSION['unique_id'] = $row['unique_id'];
+                    echo "success";
+                }else{
+                    echo "Что-то пошло не так. Пожалуйста, попробуйте еще раз!";
+                }
+            }else{
+                echo "Неверный адрес электронной почты или пароль!";
+            }
+        }else{
+            echo "$email - Этой электронной почты не существует!";
+        }
+    }else{
+        echo "Все поля ввода обязательны для заполнения!";
+    }
+	
+	
+?>
